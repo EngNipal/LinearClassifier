121a4c869bf6a924cf80ad80f16c66ba97f050f1
EngNipal <lovelas61@ya.ru>
2020-02-16 14:57:12 +0300

    Создан класс нейрона с сумматором, но пока без нелинейности.

---
+++ Neuron.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace LinearClassifier
+{
+    public class Neuron
+    {
+        // Конструктор нейрона, требующий количество входов на нейрон.
+        public Neuron (int NumberOfInputs)
+        { _numberOfInputs = NumberOfInputs; }
+        // Количество входов нейрона.
+        private static int _numberOfInputs { get; set; }
+        // Набор входных значений.
+        private List<double> _inputs = new List<double>(_numberOfInputs);
+        public List<double> Inputs
+        {
+            get
+            { return _inputs; }
+            set
+            {
+                if (value.Capacity == _inputs.Capacity)
+                {
+                    for (int i = 0; i < value.Capacity; i++)
+                    {
+                        _inputs.Add(value[i]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("А у вас ошибка! Количество входных Inputs больше, чем установлено для нейрона");
+                    // Exception
+                }
+            }
+        }
+        // Набор весов.
+        private List<double> _weights = new List<double>(_numberOfInputs);
+        public List<double> Weights
+        {
+            get
+            { return _weights; }
+            set
+            {
+                if (value.Capacity == _weights.Capacity)
+                {
+                    for (int i = 0; i < value.Capacity; i++)
+                    {
+                        _weights[i] = value[i];
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("А у вас ошибка! Количество входных Weights больше, чем установлено для нейрона");
+                    // Exception
+                }
+            }
+        }
+        // Вес смещения.
+        private double _bias { get; set; }
+        public double Bias
+        {
+            get
+            { return _bias; }
+            private set
+            { _bias = value; }
+        }
+        // Сумматор
+        internal void Summator()
+        {
+            _output = 0.0;
+            for (int i = 0; i < _weights.Capacity; i++)
+            {
+                _output += _weights[i] * _inputs[i];
+            }
+            _output += _bias;
+        }
+        // Выход нейрона
+        private double _output { get; set; }
+        public double Output
+        {
+            get { return _output; }            
+        }
+    }
+}
