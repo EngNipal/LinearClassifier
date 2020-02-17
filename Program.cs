c647b9c68fb1375e08d15b514122234e220b2ad0
EngNipal <lovelas61@ya.ru>
2020-02-17 23:52:04 +0300

    Создан куб. Определены размеры входного и выходного слоёв. Созданы слои, наполнены нейронами и рандомными весами.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,32 @@ namespace LinearClassifier
     {
         static void Main(string[] args)
         {
-            
+            Cube TrainCube = new Cube();                        // Создание куба.
+            int TaskDimension = TrainCube.State.Capacity;       // Объём входных данных.
+            const int Layer_1_Dimension = 64;                   // Размер первого слоя.
+            const int OutputDimension = 9;                      // Размер выходного слоя.
+            Random Rnd = new Random();                          // Рандомайзер.
+
+            // Создание слоёв, наполнение их нейронами и инициализация весов нейронов.
+            Layer Layer_1 = new Layer(Layer_1_Dimension, "Alone");
+            for (int i = 0; i < Layer_1_Dimension; i++)
+            {
+                Layer_1.SetOfNeurons[i] = new Neuron(TaskDimension);
+                for (int j = 0; j < TaskDimension; j++)
+                {
+                    Layer_1.SetOfNeurons[i].Weights[j] = Rnd.NextDouble();
+                }
+            }
+            Layer OutputLayer = new Layer(OutputDimension, "Out");
+            for (int i = 0; i < OutputDimension; i++)
+            {
+                OutputLayer.SetOfNeurons[i] = new Neuron(Layer_1_Dimension);
+                for (int j = 0; j < TaskDimension; j++)
+                {
+                    OutputLayer.SetOfNeurons[i].Weights[j] = Rnd.NextDouble();
+                }
+            }
+
         }
     }
 }
